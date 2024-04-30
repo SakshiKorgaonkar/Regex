58286225e2512911e57e2fe73c3c098f2f943481
SakshiKorgaonkar <korgaonkarsakshi23@gmail.com>
2024-04-30 12:07:57 +0530

    [sakshi]add : Name validation UC1

---
+++ Regex/Regex/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter user name : ");
+        string name=Console.ReadLine();
+        if(isValidName(name))
+        {
+            Console.WriteLine("Name is valid");
+        }
+        else
+        {
+            Console.WriteLine("Name not valid");
+        }
+    }
+     static bool isValidName(string name)
+    {
+        string pattern = @"^[A-Z].{2,}$";
+        return Regex.IsMatch(name, pattern);
+    }
+}
